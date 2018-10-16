14a55e691aed9c8f3dba6eb74d7a5388fe6c2cec
LiselotteEL <lise1066@edu.easj.dk>
2018-10-16 20:50:46 +0200

    Multiplication class and test with no fake it added

--- Test/RegnemaskingTest_multiplication/MultiplicationTest.java
+++ Test/RegnemaskingTest_multiplication/MultiplicationTest.java
@@ -1,5 +1,6 @@
 package RegnemaskingTest_multiplication;
 
+import Regnemaskine.Multiplication;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,10 +8,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MultiplicationTest {
 
-
+    // fake it test
     @Test
-
-    public void multiplication(){
+    public void multiplication1(){
 
         double a = 2;
         double b = 4;
@@ -18,6 +18,16 @@ public class MultiplicationTest {
         assertEquals(8, a * b);
 
     }
+
+    // test uden fake it
+    @Test
+    public void multiplication2(){
+
+        Multiplication multiplication = new Multiplication();
+
+        assertEquals(10, multiplication.simpleMultiplaction(5,2));
+    }
+
 }
 
 
---
+++ src/Regnemaskine/Multiplication.java
@@ -0,0 +1,13 @@
+package Regnemaskine;
+
+public class Multiplication {
+
+    //laver en metode som kan gange to tal
+
+    public double simpleMultiplaction(double a, double b){
+
+        double result = a * b;
+
+        return result;
+    }
+}
