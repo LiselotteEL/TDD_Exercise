d9018ae21ef4832149df4f9aa4765c6fe61f8d0a
LiselotteEL <lise1066@edu.easj.dk>
2018-10-16 20:45:26 +0200

    Multiplication fake it test

---
+++ Test/RegnemaskingTest_multiplication/MultiplicationTest.java
@@ -0,0 +1,24 @@
+package RegnemaskingTest_multiplication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MultiplicationTest {
+
+
+    @Test
+
+    public void multiplication(){
+
+        double a = 2;
+        double b = 4;
+
+        assertEquals(8, a * b);
+
+    }
+}
+
+
+
