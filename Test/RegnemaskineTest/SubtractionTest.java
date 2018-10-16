c896b7a2a2c2b1fd7da300cda98c99d65f4f6d20
LiselotteEL <lise1066@edu.easj.dk>
2018-10-16 20:33:28 +0200

    Subtraction fake it test

---
+++ Test/RegnemaskineTest/SubtractionTest.java
@@ -0,0 +1,22 @@
+package RegnemaskineTest;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SubtractionTest {
+
+
+    @Test
+    public void simpleSubtract1() {
+
+        double a = 10;
+        double b = 5;
+
+
+        assertEquals(5,a - b);
+
+    }
+
+
+}
