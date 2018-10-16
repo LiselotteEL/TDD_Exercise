f8df0f32e0bfbbe672caf8cdaf031047288e1364
LiselotteEL <lise1066@edu.easj.dk>
2018-10-16 20:30:06 +0200

    Addition class and no fake test

--- Test/Regnemaskine_Test/addition/AdditionTest.java
+++ Test/Regnemaskine_Test/addition/AdditionTest.java
@@ -1,25 +1,41 @@
 package Regnemaskine_Test.addition;
 
 
+import Regnemaskine.Addition;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public  class AdditionTest{
 
-    private double tempA;
-    private double tempB;
-    private double tempC;
-    private double tempD;
 
     @Test
-    void simpleAdd(){
+    void simpleAdd1(){
         //fake it implementation
 
         double a = 4;
         double b = 3;
 
+
+
         assertEquals(7,a + b);
     }
+
+
+    @Test
+    void simpleAdd2(){
+        //test uden fake it
+
+        //opretter en instans a typen Addition for at kunne anvende klassens metoder
+        Addition addition = new Addition();
+
+        /*anvender assertEquals
+        add.simpleAddition betyder at den skal finde metoden simpleAddition() i klassen Addition (jeg kunne sagtens have navngivet den alt andet end 'addition'
+        min metode i Addition skal kunne håndtere alle tal og lægge dem sammen
+        denne test skal kunne tage imod alle tal og sammenligne dem med alle "expected" resultater
+        * */
+        assertEquals(7,addition.simpleAddition(3,4));
+    }
+
 
 }
---
+++ src/Regnemaskine/Addition.java
@@ -0,0 +1,16 @@
+package Regnemaskine;
+
+public class Addition {
+
+
+
+    //laver en metode, som kan lægge to tal sammen
+    public double simpleAddition(double a, double b){
+
+        double result = a + b;
+
+        return result;
+
+    }
+
+}
