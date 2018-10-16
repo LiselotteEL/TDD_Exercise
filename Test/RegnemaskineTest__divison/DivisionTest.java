0b98c6f4298f3830eb7ff7007ac063b7692819e4
LiselotteEL <lise1066@edu.easj.dk>
2018-10-16 21:03:59 +0200

    Division class and test with no fake it added

--- Test/RegnemaskineTest__divison/DivisionTest.java
+++ Test/RegnemaskineTest__divison/DivisionTest.java
@@ -1,5 +1,6 @@
 package RegnemaskineTest__divison;
 
+import Regnemaskine.Division;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,11 +10,25 @@ public class DivisionTest {
 
     //fake it test
     @Test
-    public void multiplication(){
+    public void division1(){
 
         double a = 10;
         double b = 5;
 
         assertEquals(2, a / b);
     }
+
+    //test with no fake it
+    @Test
+    public void division2(){
+
+        Division division = new Division();
+
+        assertEquals(10,division.simpleDivision(100,10));
+    }
+
+
+
+
+
 }
---
+++ src/Regnemaskine/Division.java
@@ -0,0 +1,13 @@
+package Regnemaskine;
+
+public class Division {
+
+    //laver en metode som kan dividere to tal med hinanden
+    public double simpleDivision(double a, double b){
+
+        double result = (a / b);
+
+        return result;
+
+    }
+}
