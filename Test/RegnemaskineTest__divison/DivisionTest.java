4fa39e22c9e3b97478583150061fa711de968975
LiselotteEL <lise1066@edu.easj.dk>
2018-10-16 20:53:59 +0200

    Division fake it test

---
+++ Test/RegnemaskineTest__divison/DivisionTest.java
@@ -0,0 +1,19 @@
+package RegnemaskineTest__divison;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class DivisionTest {
+
+    //fake it test
+    @Test
+    public void multiplication(){
+
+        double a = 10;
+        double b = 5;
+
+        assertEquals(2, a / b);
+    }
+}
